bb67541245be6660b28fe6d5d0ca41aacb6acd58
bryanjustice <bryan.j.justice@gmail.com>
2022-11-03 23:13:33 -0400

    added comments

--- OpticsLibraries/optics_functions.py
+++ OpticsLibraries/optics_functions.py
@@ -8,11 +8,13 @@ def refractedAngle(n1: float, n2: float, incident: float) -> float:
     medium of index n2 at some angle of incidence in degrees.
     Returns angle in degrees.
     """
+    # convert input angle from degrees to radians
     incident_rad = math.degrees(incident)
+
+    # calculate angle of refraction in radians
     refracted_rad = math.asin((n1/n2)*math.sin(incident_rad))
+
+    # convert output back to degrees before returning
     refracted = math.radians(refracted_rad)
 
     return refracted
-
-
-
